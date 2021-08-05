5152e001077fa9a81137d0d4c2fabedd0911d046
rdwentmp <maaarek7823@gmail.com>
2021-08-05 12:39:05 +0200

    initialize project

---
+++ src/com/rtjvm/scala/oop/filesystem/Filesystem.scala
@@ -0,0 +1,28 @@
+package com.rtjvm.scala.oop.filesystem
+
+import java.util.Scanner
+
+import com.rtjvm.scala.oop.commands.Command
+import com.rtjvm.scala.oop.files.Directory
+
+/**
+  * Created by Daniel on 28-Oct-17.
+  */
+object Filesystem extends App {
+
+  val root = Directory.ROOT
+
+  // [1, 2, 3, 4]
+  /*
+   0 (op) 1 => 1
+   1 (op) 2 => 3
+   3 (op) 3 => 6
+   6 (op) 4 => your last value, 10
+
+   List(1,2,3,4).foldLeft(0)((x, y) => x + y)
+    */
+  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
+    currentState.show
+    Command.from(newLine).apply(currentState)
+  })
+}
---
+++ src/com/rtjvm/scala/oop/filesystem/State.scala
@@ -0,0 +1,25 @@
+package com.rtjvm.scala.oop.filesystem
+
+import com.rtjvm.scala.oop.files.Directory
+
+/**
+  * Created by Daniel on 28-Oct-17.
+  */
+class State(val root: Directory, val wd: Directory, val output: String) {
+
+  def show: Unit = {
+    println(output)
+    print(State.SHELL_TOKEN)
+  }
+
+  def setMessage(message: String): State =
+    State(root, wd, message)
+
+}
+
+object State {
+  val SHELL_TOKEN = "$ "
+
+  def apply(root: Directory, wd: Directory, output: String = ""): State =
+    new State(root, wd, output)
+}
